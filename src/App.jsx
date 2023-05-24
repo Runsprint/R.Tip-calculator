442fa6be2e50f16576dab23ffe383f34d1aa5e50
gvantsa kapanadze <gvantsakapanadze95@gmail.com>
2023-05-24 23:23:22 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+import "../public/icon-dollar.svg";
+import "../public/icon-person.svg";
+import "../public/logo.svg";
+function App() {
+  return (
+    <>
+      <h1 className="title">
+        SPLI
+        <br />
+        TER
+      </h1>
+      <div clasName="main-div">
+        <div className="white-div"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
